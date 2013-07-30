658d00cccbfec2172f37ea40d80027fba8cd49bf
Valtis <Omistaja@Omistaja-PC.(none)>
2013-07-30 21:29:26 +0300

    Humdumdum. Ensimmäiset tiedostot.

---
+++ src/Kayttoliittyma/Kayttoliittyma.java
@@ -0,0 +1,13 @@
+
+
+package Kayttoliittyma;
+
+
+public class Kayttoliittyma implements Runnable {
+    
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ src/main/main.java
@@ -0,0 +1,12 @@
+package main;
+
+import Kayttoliittyma.Kayttoliittyma;
+
+public class main {
+
+    static public void main(String[] args) {
+        Kayttoliittyma liittyma = new Kayttoliittyma();
+        liittyma.run();
+        
+    }
+}
